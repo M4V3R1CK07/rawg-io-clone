ecde48767a3334d7d3cdb79a21ef39449dfdc3c0
Maverick <Maverick>
2024-09-13 16:32:58 +0530

    Sorted the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,9 +22,10 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
       },
     },
-    [gameQuery.genre?.id, gameQuery.platform?.id]
+    [gameQuery]
   );
 
 export default useGames;
