6b47d72af4aea3db8578245a4b5bc3b7d4a553b3
M4V3R1CK07 <aditya2708kumar@gmail.com>
2024-09-14 19:56:02 +0530

    Updated page title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Genre } from "./hooks/useGenre";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
