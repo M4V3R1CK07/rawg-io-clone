688dd95be7edb796be26877630e09301f6689afb
Maverick <Maverick>
2024-09-14 17:37:40 +0530

    Built search box

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchBox from "./SearchBox";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize={"50px"} />
+      <SearchBox />
       <ColorModeSwitch />
     </HStack>
   );
